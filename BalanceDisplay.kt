7f387326652e0016861a14c857642f04046233c8
Ghostdevol <karmastudiosbeatz@gmail.com>
2026-02-27 01:59:33 -0600

    Update BalanceDisplay.kt

--- BalanceDisplay.kt
+++ BalanceDisplay.kt
@@ -1,1 +1,12 @@
 
+package com.yourdomain.slotmachine.composables
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.yourdomain.slotmachine.GameViewModel
+
+@Composable
+fun BalanceDisplay(game: GameViewModel) {
+    Text("Balance: ${game.balance}", color = Color.Green)
+}
